a0e479c1b45574ff7047eeace1da9f9b3dede7ce
LEELAVENKATASAISUMANTHE <1by23cs111@bmsit.in>
2026-02-20 10:19:37 +0530

    refactor: Simplify server startup by removing Kafka connection and using default port

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
 import app from './utils/app.js';
 import logger from './utils/logger.js';
-import { connectKafka } from './utils/kafka.js';
+  
+const PORT = process.env.PORT || 3225; // Default port 3225
 
-async function startServer() {
-  await connectKafka(); // connect Kafka FIRST
-  const PORT = process.env.PORT || 4000;
-  app.listen(PORT, () => {
-    console.log(`🚀 Server running on port ${PORT}`);
-    console.log(`http://localhost:${PORT}`);
-    logger.info(`Server started on port ${PORT}`);
-  });
-}
+// Start the server
 
-startServer();
+app.listen(PORT, () => {
+  console.log(`🚀 Server is running on port ${PORT}`);
+  console.log(`http://localhost:${PORT}`);
+  logger.info(`Server started on port ${PORT} `);
+});
 
